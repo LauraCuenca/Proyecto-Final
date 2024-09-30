d27daee1120432cb84001b5f0ef9d18ff2211bc1
Cuenca Laura Antonina <laura.cuenca1@gmail.com>
2024-09-30 18:34:38 -0300

    agrego cosas de saving a la bd

--- db/associations.js
+++ db/associations.js
@@ -1,6 +1,9 @@
 const User = require ('./models/user.js');
 const Task = require ('./models/task.js');
+const Saving = require ('./models/saving.js');
 
 
 User.hasMany(Task)
+User.hasMany(Saving)
 Task.belongsTo(User)
+Saving.belongsTo(User)
